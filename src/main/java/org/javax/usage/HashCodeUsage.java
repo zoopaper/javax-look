9323bdf6d3a7045b6b7a12eaac413af881482b75
krisjin <krisibm@163.com>
2015-04-08 11:15:56 +0800

    hashcode

---
+++ src/main/java/org/javax/usage/HashCodeUsage.java
@@ -0,0 +1,85 @@
+package org.javax.usage;
+
+import java.util.Arrays;
+
+/**
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/4/8
+ * Time: 10:20
+ */
+public class HashCodeUsage {
+
+
+    static class User {
+        private String userName;
+        private Long userId;
+        private String[] address;
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public void setUserName(String userName) {
+            this.userName = userName;
+        }
+
+
+        @Override
+        public int hashCode() {
+            return userId.hashCode() * userName.hashCode() * Arrays.hashCode(address);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (!(obj instanceof User)) {
+                return false;
+            }
+            User user = (User) obj;
+
+            return userName.equals(user.getUserName()) && userId == user.getUserId() && address.equals(user.getAddress());
+        }
+
+        public void setAddress(String[] address) {
+            this.address = address;
+        }
+
+        public String[] getAddress() {
+            return address;
+        }
+
+        public void setUserId(Long userId) {
+            this.userId = userId;
+        }
+
+        public Long getUserId() {
+            return userId;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        String[] strArr=new String[2];
+
+
+        User user1 = new User();
+        user1.setUserId(100L);
+        user1.setUserName("krisjin");
+        user1.setAddress(strArr);
+
+
+
+        strArr[0]="test";
+        User user2 = new User();
+        user2.setUserId(100L);
+        user2.setUserName("krisjin");
+        user2.setAddress(strArr);
+
+        System.out.println("User1 equals User2 :" + user1.equals(user2));
+        System.out.println("User1 hashcode :" + user1.hashCode());
+        System.out.println("User2 hashcode :" +user2.hashCode());
+
+    }
+}
