000ef8fc542434cd7cf51fb54d4922eb592a4291
krisjin <krisibm@163.com>
2015-04-15 18:55:00 +0800

    annotation

---
+++ src/main/java/org/javax/annotation/Key.java
@@ -0,0 +1,15 @@
+package org.javax.annotation;
+
+/**
+ * <p/>
+ * User : krisibm@163.com
+ * Date: 2015/4/15
+ * Time: 15:43
+ */
+public class Key {
+
+    public <T> Class<T> get(){
+
+        return null;
+    }
+}
